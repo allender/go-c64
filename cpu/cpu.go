86f84e5cb5b28cd13d284e7147d53b51865620a5
Mark Allender <mark.allender@gmail.com>
2017-10-27 08:16:28 -0500

    Add addressing modes

--- cpu/cpu.go
+++ cpu/cpu.go
@@ -1,5 +1,7 @@
 package cpu
 
+import "github.com/allender/go-c64/memory"
+
 // Cpu is the main structure for the 6502 emulation
 type Cpu struct {
 
@@ -20,4 +22,65 @@ type Cpu struct {
 
 	// SR is the status register
 	SR uint8
+}
+
+// Addr_mode_t is a function type for addressing modes
+type Addr_mode_t func(*Cpu) uint16
+
+func (c *Cpu) Imm() uint16 {
+	return c.PC
+}
+
+func (c *Cpu) Zpg() uint16 {
+	low := memory.Read(c.PC)
+	return uint16(low)
+}
+
+func (c *Cpu) Zpx() uint16 {
+	addr := uint16(memory.Read(c.PC))
+	return (addr + uint16(c.X)) & 0xff
+}
+
+func (c *Cpu) Zpy() uint16 {
+	addr := uint16(memory.Read(c.PC))
+	return (addr + uint16(c.Y)) & 0xff
+}
+
+func (c *Cpu) Inx() uint16 {
+	addr := uint16(memory.Read(c.PC)) + uint16(c.X)
+	low := memory.Read(addr)
+	high := memory.Read(addr + 1)
+	return (uint16(high) << 8) | uint16(low)
+}
+
+func (c *Cpu) Iny() uint16 {
+	addr := uint16(memory.Read(c.PC))
+	low := memory.Read(addr)
+	high := memory.Read(addr + 1)
+	return ((uint16(high) << 8) | uint16(low)) + uint16(c.Y)
+}
+
+func (c *Cpu) Abs() uint16 {
+	low := memory.Read(c.PC)
+	high := memory.Read(c.PC + 1)
+	return (uint16(high) << 8) | uint16(low)
+}
+
+func (c *Cpu) Abx() uint16 {
+	low := memory.Read(c.PC)
+	high := memory.Read(c.PC + 1)
+	return (uint16(high) << 8) | uint16(low) + uint16(c.X)
+}
+
+func (c *Cpu) Aby() uint16 {
+	low := memory.Read(c.PC)
+	high := memory.Read(c.PC + 1)
+	return (uint16(high) << 8) | uint16(low) + uint16(c.Y)
+}
+
+func (c *Cpu) Ind() uint16 {
+	low := memory.Read(c.PC)
+	high := memory.Read(c.PC + 1)
+	addr := (uint16(high) << 8) | uint16(low)
+	return uint16(memory.Read(addr)) | (uint16(memory.Read(addr+1)) << 8)
 }
